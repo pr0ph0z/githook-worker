3bf14b4188708a33887c5aeb08b883104418f4d3
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-04-09 06:51:29 +0700

    Update logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,15 +2,15 @@ const moment = require('moment')
 const Logger = function () {}
 
 Logger.prototype.info = function (logText) {
-    console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Info: ${logText}`)
+  console.log(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] Info: ${logText}`)
 }
 
 Logger.prototype.error = function (logText) {
-    console.log(
-        `[${moment().format('DD-MM-YYYY HH:mm:ss')}] Error: ${
-            logText instanceof Error ? logText.message : logText
-        }`
-    )
+  console.log(
+    `[${moment().format('DD-MM-YYYY HH:mm:ss')}] Error: ${
+      logText instanceof Error ? logText.message : logText
+    }`
+  )
 }
 
 module.exports = new Logger()
