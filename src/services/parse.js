4473cdc41cbebbc9702a749c9358fcabcf5ca60d
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-04-09 23:36:35 +0700

    Add parse service

---
+++ src/services/parse.js
@@ -0,0 +1,79 @@
+const { findUserByGithubId } = require('../services/user')
+const { getChannel } = require('../services/channel')
+
+const parseMessage = async (message) => {
+  const messages = {}
+  let assignee, reviewer
+  message = JSON.parse(message)
+  message.type = message.type.replace('_', ' ')
+  const article = message.type === 'issue' ? 'an' : 'a'
+
+  const sender = await findUserByGithubId(message.sender.github_id)
+
+  if (message.assignee !== undefined) {
+    assignee = await findUserByGithubId(message.assignee.github_id)
+  }
+  if (message.reviewer !== undefined) {
+    reviewer = await findUserByGithubId(message.reviewer.github_id)
+  }
+  const channel = await getChannel()
+
+  switch (message.action) {
+    case 'create':
+      messages.server = {
+        id: channel.id,
+        message: `<@!${sender.discord.id}> just created ${article} ${message.type} in **#${message.number}** named \`${message.title}\`. ${message.url}`
+      }
+      break
+    case 'closed':
+      messages.server = {
+        id: channel.id,
+        message: `<@!${sender.discord.id}> just closed ${article} ${message.type} in **#${message.number}** named \`${message.title}\`. ${message.url}`
+      }
+      break
+    case 'assigned_issue':
+      messages.direct_message = {
+        id: assignee.discord.id,
+        message: `Hi ${message.assignee.username}, you've been assigned to ${article} ${message.type} in **#${message.number}** named \`${message.title}\`. ${message.url}`
+      }
+      break
+    case 'assigned_pull_request':
+      messages.direct_message = {
+        id: assignee.discord.id,
+        message: `Hi ${message.assignee.username}, you've been assigned to ${article} ${message.type} in **#${message.number}** named \`${message.title}\`. ${message.url}`
+      }
+      break
+    case 'review_pull':
+      messages.direct_message = {
+        id: reviewer.discord.id,
+        message: `Hi ${message.reviewer.username}, you've been requested to review ${article} ${message.type} in **#${message.number}** named \`${message.title}\` by ${sender.github.username}. ${message.url}`
+      }
+      break
+    case 'changes_requested':
+      messages.direct_messages = []
+      for (const assignee of message.assignees) {
+        const assigneeUser = await findUserByGithubId(assignee.github_id)
+        messages.direct_messages.push({
+          id: assigneeUser.discord.id,
+          message: `Hi ${assignee.username}, your subscribed pull request in **#${message.number}** named \`${message.title}\` has been requested to change by ${sender.github.username}. ${message.url}`
+        })
+      }
+      break
+    case 'approved':
+      messages.direct_messages = []
+      for (const assignee of message.assignees) {
+        const assigneeUser = await findUserByGithubId(assignee.github_id)
+        messages.direct_messages.push({
+          id: assigneeUser.discord.id,
+          message: `Hi ${assignee.username}, your subscribed pull request in **#${message.number}** named \`${message.title}\` has been approved by ${sender.github.username}. ${message.url}`
+        })
+      }
+      break
+  }
+
+  return messages
+}
+
+module.exports = {
+  parseMessage
+}
