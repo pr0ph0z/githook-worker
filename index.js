0987d1b4decd1614d9724e849294aec61c0a57d2
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-04-09 23:37:57 +0700

    Update regex target string, update discord id to be replaced, and move amqp connect sequence when discord bot is  ready

--- index.js
+++ index.js
@@ -6,30 +6,48 @@ const logger = require('./src/utils/logger')
 const github = require('./src/services/github')
 const user = require('./src/services/user')
 const channel = require('./src/services/channel')
+const { parseMessage } = require('./src/services/parse')
 
 const client = new Client()
-
-amqp.connectToAmqp()
-  .then(channel => {
-    logger.info('AMQP connected!')
-    return channel
-  })
-  .then(channel => {
-    logger.info('Consuming...')
-    channel.consume('hooks', async msg => {
-      client.emit('hooks', msg)
-    })
-  })
 
 mongo.createConnection()
 
 client.on('ready', () => {
   logger.info('Discord bot is ready!')
+  amqp.connectToAmqp()
+    .then(channel => {
+      logger.info('AMQP connected!')
+      return channel
+    })
+    .then(channel => {
+      logger.info('Consuming...')
+      channel.consume('hooks', async msg => {
+        const messages = await parseMessage(msg.content.toString())
+
+        if (messages.server !== undefined) {
+          client.channels.cache.get(messages.server.id).send(messages.server.message)
+        }
+        if (messages.direct_message !== undefined) {
+          const user = await client.users.fetch(messages.direct_message.id)
+          const dm = await user.createDM()
+          await dm.send(messages.direct_message.message)
+        }
+        if (messages.direct_messages !== undefined) {
+          for (const directMessage of messages.direct_messages) {
+            const user = await client.users.fetch(directMessage.id)
+            const dm = await user.createDM()
+            await dm.send(directMessage.message)
+          }
+        }
+
+        channel.ack(msg)
+      })
+    })
 })
 
 client.on('message', async message => {
   if (message.content.startsWith('/assign')) {
-    const [commands] = [...'/assign pr0ph0z <@!414577521263247367>'.matchAll(/(\/assign)\s(.*)\s<@!(.*)>/g)]
+    const [commands] = [...message.content.matchAll(/(\/assign)\s(.*)\s(.*)/g)]
     if (commands.length === 4) {
       const [,, githubUsername, discordId] = commands
       const githubUser = await github.getUserIdByUsername(githubUsername)
@@ -40,7 +58,7 @@ client.on('message', async message => {
             username: githubUsername
           },
           discord: {
-            id: discordId
+            id: discordId.replace(/[<@!>]/g, '')
           }
         }
         await user.updateUser(payload)
