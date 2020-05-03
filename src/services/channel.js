e55ec69e3414a4f64a69ceb0b7ffa02c27ebf3de
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-05-03 16:32:58 +0700

    Remove error handler in all services

--- src/services/channel.js
+++ src/services/channel.js
@@ -1,29 +1,20 @@
 const Channel = require('../models/channel')
-const logger = require('../utils/logger')
 
 const updateChannel = async ({ id, name }) => {
-  try {
-    const channel = await Channel.findOne()
-    const query = { id, name }
-    if (!channel) {
-      await Channel.create(query)
-    } else {
-      await Channel.updateOne({}, query)
-    }
-  } catch (error) {
-    logger.error(error)
+  const channel = await Channel.findOne()
+  const query = { id, name }
+
+  if (!channel) {
+    await Channel.create(query)
+  } else {
+    await Channel.updateOne({}, query)
   }
 }
 
 const getChannel = async () => {
-  try {
-    const channel = await Channel.findOne()
+  const channel = await Channel.findOne()
 
-    return channel
-  } catch (error) {
-    logger.error(error)
-    return false
-  }
+  return channel
 }
 
 module.exports = {
--- src/services/user.js
+++ src/services/user.js
@@ -1,30 +1,20 @@
 const User = require('../models/user')
-const logger = require('../utils/logger')
 
 const updateUser = async ({ github, discord }) => {
-  try {
-    await User.updateOne({
-      $or: [
-        { 'github.username': github.username },
-        { 'discord.id': discord.id }
-      ]
-    },
-    { github, discord },
-    { upsert: true })
-  } catch (error) {
-    logger.error(error)
-  }
+  await User.updateOne({
+    $or: [
+      { 'github.username': github.username },
+      { 'discord.id': discord.id }
+    ]
+  },
+  { github, discord },
+  { upsert: true })
 }
 
 const findUserByGithubId = async (githubId) => {
-  try {
-    const user = await User.findOne({ 'github.id': githubId })
+  const user = await User.findOne({ 'github.id': githubId })
 
-    return user
-  } catch (error) {
-    logger.error(error)
-    return false
-  }
+  return user
 }
 
 module.exports = {
